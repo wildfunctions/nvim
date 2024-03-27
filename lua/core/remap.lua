54f75882f7ba7368ba223ef39fdb6392816fe5c3
wildfunctions <dev@brittkim.com>
2024-03-27 16:07:19 -0500

    fixed J,K keymap

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,7 +1,7 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ">+1<CR>gv=gv")
-vim.keymap.set("v", "K", "<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 
