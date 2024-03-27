5dcecb6f49f116f3e43ef4656a8c397ec1a2c9a2
wildfunctions <dev@brittkim.com>
2024-03-27 16:07:19 -0500

    remove packer and use lazy nvim

--- init.lua
+++ init.lua
@@ -1,1 +1,36 @@
 require("core")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	'neovim/nvim-lspconfig',
+	'hrsh7th/cmp-nvim-lsp',
+	'hrsh7th/nvim-cmp',
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
+	},
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+	'theprimeagen/harpoon',
+	'williamboman/mason.nvim',
+	'williamboman/mason-lspconfig.nvim'
+})
