a10dbe415325efe66674d5cb0e88bd5cc2c0926b
wildfunctions <dev@brittkim.com>
2024-03-27 16:07:19 -0500

    fixed C-d, C-u though the consequence is that scrolling nmap is bricked

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -6,6 +6,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "<leader>y", "\"+y")
