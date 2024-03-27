289a6c50c5185de52b3c875e710742fa6be38b92
wildfunctions <dev@brittkim.com>
2024-03-27 16:07:19 -0500

    remaps and sets

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,2 +1,11 @@
-vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ">+1<CR>gv=gv")
+vim.keymap.set("v", "K", "<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-d>zz")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
