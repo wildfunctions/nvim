fcb0fe6ec20168a02e0c6d82549af47b1a61a26c
wildfunctions <3144575+wildfunctions@users.noreply.github.com>
2024-04-09 20:27:42 -0500

    lua snip

--- init.lua
+++ init.lua
@@ -71,6 +71,10 @@ require("lazy").setup({
   },
   'neovim/nvim-lspconfig',
   'hrsh7th/cmp-nvim-lsp',
+  {
+    "L3MON4D3/LuaSnip",
+    lazy = false
+  },
   'hrsh7th/nvim-cmp',
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
