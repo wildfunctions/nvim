a56d3d7db768dee822860103d9f51343e0b8702e
wildfunctions <3144575+wildfunctions@users.noreply.github.com>
2024-04-17 19:11:00 -0500

    copilot enable/disable shortkeys

---
+++ after/plugin/copilot.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>ce", function() vim.cmd('Copilot enable'); print("Copilot enabled") end, { noremap = true})
+vim.keymap.set("n", "<leader>cd", function() vim.cmd('Copilot disable'); print("Copilot disabled") end, { noremap = true})
+ 
