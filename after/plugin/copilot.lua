08b1ed5c71cdd753ec44df120163cab7db080ec4
wildfunctions <3144575+wildfunctions@users.noreply.github.com>
2024-04-17 19:12:07 -0500

    after merge

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,3 +1,2 @@
 vim.keymap.set("n", "<leader>ce", function() vim.cmd('Copilot enable'); print("Copilot enabled") end, { noremap = true})
 vim.keymap.set("n", "<leader>cd", function() vim.cmd('Copilot disable'); print("Copilot disabled") end, { noremap = true})
- 
